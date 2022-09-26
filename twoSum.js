38254f1f99cba4f324d3076c663f706b10b82c1b
Eugene <lamew128@gmail.com>
2022-09-26 17:04:38 -0400

    update 2sum

--- twoSum.js
+++ twoSum.js
@@ -4,15 +4,21 @@
  * @return {number[]}
  */
  var twoSum = function(nums, target) {
-  if (nums.length === 2) {
-      return [0,1];
-  }
-  
-  for (let i = 0; i < nums.length; i++) {
-      let diff = target - nums[i];
-      if (nums.includes(diff) && nums.indexOf(diff) !== i) {
-          return [i, nums.indexOf(diff)];
-      }
-  }
-  return null;
+    if (nums.length === 2) {
+        return [0,1];
+    }
+    let map = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        if (map.has(diff)) {
+            return [i, map.get(diff)];
+        } else {
+            map.set(nums[i], i);
+        }
+        //if (nums.includes(diff) && nums.indexOf(diff) !== i) {
+        //    return [i, nums.indexOf(diff)];
+        //}
+    }
+    return null;
 };
